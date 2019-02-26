2eb6ad9ec9f75f9f90d441003ad6dec87e63b234
garbely <patrick.garbely@students.hevs.ch>
2019-02-26 16:54:53 +0100

    Mainpage Listview - Example Data

--- app/src/main/java/com/example/tv_shows/MainActivity.java
+++ app/src/main/java/com/example/tv_shows/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.tv_shows;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listview = (ListView) findViewById(R.id.listview);
+
+        String [] shows = getResources().getStringArray(R.array.shows_array);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, shows);
+
+        listview.setAdapter(adapter);
     }
 }
