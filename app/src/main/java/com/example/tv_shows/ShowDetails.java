a1d05bd9e4a9797c4b4bb23d1e0de6fa6567feff
kalbermattenx <xavier.kalbermatten@students.hevs.ch>
2019-03-11 11:09:41 +0100

    Beide Modify-Seiten (Episode- & ShowModify) erstellt

--- app/src/main/java/com/example/tv_shows/ShowDetails.java
+++ app/src/main/java/com/example/tv_shows/ShowDetails.java
@@ -1,13 +1,44 @@
 package com.example.tv_shows;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ShowDetails extends AppCompatActivity {
+
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_details);
+
+        listview = (ListView) findViewById(R.id.listview);
+
+        String [] episodes = getResources().getStringArray(R.array.episodes_array);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, episodes);
+
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener (new AdapterView.OnItemClickListener(){
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent myintent = new Intent(view.getContext(), ShowModify.class);
+                startActivityForResult(myintent, 0);
+            }
+        });
+
+
     }
+
+
+
+
+
+
 }
