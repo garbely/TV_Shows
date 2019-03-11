5038b37a19f7b00bc2eae93bcb3d5ec45a19ad6b
garbely <patrick.garbely@students.hevs.ch>
2019-03-11 17:57:26 +0100

    ActionBar - Icons uns ItemClickListener hinzugefügt

--- app/src/main/java/com/example/tv_shows/EpisodeDetails.java
+++ app/src/main/java/com/example/tv_shows/EpisodeDetails.java
@@ -1,7 +1,10 @@
 package com.example.tv_shows;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class EpisodeDetails extends AppCompatActivity {
 
@@ -10,4 +13,33 @@ public class EpisodeDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_episode_details);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_actionbar, menu);
+
+        MenuItem item1 = menu.findItem(R.id.add);
+        item1.setVisible(false);
+        this.invalidateOptionsMenu();
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent intent = getIntent();
+
+        switch(item.getItemId()){
+            case R.id.edit:
+                intent = new Intent(EpisodeDetails.this, EpisodeModify.class);
+                break;
+            case R.id.delete:
+                // Delete Episode
+                break;
+        }
+        startActivityForResult(intent, 0);
+        return true;
+    }
+
 }
--- app/src/main/java/com/example/tv_shows/ShowDetails.java
+++ app/src/main/java/com/example/tv_shows/ShowDetails.java
@@ -3,6 +3,8 @@ package com.example.tv_shows;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -28,17 +30,35 @@ public class ShowDetails extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent myintent = new Intent(view.getContext(), ShowModify.class);
+                Intent myintent = new Intent(view.getContext(), EpisodeDetails.class);
                 startActivityForResult(myintent, 0);
             }
         });
-
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        Intent intent = getIntent();
 
-
-
+        switch(item.getItemId()){
+            case R.id.add:
+                intent = new Intent(ShowDetails.this, EpisodeModify.class);
+                break;
+            case R.id.edit:
+                intent = new Intent(ShowDetails.this, ShowModify.class);
+                break;
+            case R.id.delete:
+                // Delete Show
+                break;
+        }
+        startActivityForResult(intent, 0);
+        return true;
+    }
 }
